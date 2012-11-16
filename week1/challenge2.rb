bae5b18f0cebe493434ea50a17a572379a3507ee
Rudi Luzar <rudi@rudicode.com>
2012-11-16 14:03:56 -0500

    week1 ch2 -- fix issue#2 - refactored main input loop

--- week1/challenge2.rb
+++ week1/challenge2.rb
@@ -1,8 +1,13 @@
 require File.expand_path('../convert', __FILE__)
 
+def display result, units
+  puts "#{ result } #{ units.to_s.capitalize }"
+end
+
 puts <<LIST_INSTRUCTIONS
 
 Temperature converter.
+
 Input the temperature you wish to convert using the following syntax:
 
 32 F to C        converts 32 Fahrenheit to Celsius
@@ -15,28 +20,23 @@ LIST_INSTRUCTIONS
 
 input = ""
 
-until input[0] == 'q'
+
+while input[0] != 'q'
   
   print "\nConvert: "
   input = gets.chomp.downcase.split(' ')
-
-   # puts "Converting #{ input } "  #debuging input
-  
-  #if input[0] == 'q'
-  #  break
-  #end
-  
-  from = input[1]
-  to = input[3]
+  break if input[0] == 'q'
+    # puts "Converting #{ input } "  #debuging input
+  from = input[1].to_s
+  to = input[3].to_s
   temperature = input[0].to_f
-  result = ""
   
-  result = convert(temperature, from, to)
+  display convert(temperature, from, to), to
     
-  if result.class != Float
-    puts "\nError: #{ result }\n *** Sorry I do not know what you mean, by #{ input.join(' ') }\n\n"
-  else 
-    puts "#{ result } #{ to.to_s.capitalize }"
-  end
+  # if result.class != Float
+  #   puts "\nError: #{ result }\n *** Sorry I do not know what you mean, by #{ input.join(' ') }\n\n"
+  # else 
+  #   puts "#{ result } #{ to.to_s.capitalize }"
+  # end
 
 end
