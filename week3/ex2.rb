54f447adc06e078c2495640a8cdc2737c1554301
Rudi Luzar <rudi@rudicode.com>
2012-11-19 17:13:04 -0500

    week3 ex2 -- Updated to read and write one line at a time

--- week3/ex2.rb
+++ week3/ex2.rb
@@ -1,18 +1,54 @@
 #!/usr/bin/env ruby
 #
-def update_word str
-  str.gsub("word", "inserted word")
+def insert_word str, find, insert
+  output = "#{insert} #{find}"
+  str.gsub(find, output)
 end
+
+def update_file filename, find, insert
+  temp_file_name = "temp.tmp"
+  puts "Updating file: #{ filename } "
+
+  file_in  = File.new(filename, "r")
+  file_out = File.new(temp_file_name, "w")
+
+  while line_in = file_in.gets
+    file_out.puts(insert_word(line_in, find, insert))
+  end
+
+  file_in.close
+  file_out.close
+
+  File.delete filename
+  File.rename(temp_file_name, filename)
+end
+
 #
+if ARGV.length != 3
+  puts ARGV.length
+  puts <<LIST_INSTRUCTIONS
+
+Inserts given text before each occurance of found text.
+
+Usage: ex2.rb FILE find insert
+
+       FILE       the file to update
+       find       the word to find in file
+       insert     the word that replaces each occurance of find
+
+example:
+         ex2.rb ex2-words.txt word inserted
+
+LIST_INSTRUCTIONS
+  exit 1
+end
+
 filename = ARGV[0]
-puts "Updating file: #{ filename } "
+find     = ARGV[1]
+insert   = ARGV[2]
 #
 if File.exist?(filename)
-  file = File.new(filename, "r+")
-  text_out = update_word(file.read)
-  file.rewind
-  file.write(text_out)
-  file.close
+  update_file(filename, find, insert)
 else
   puts "File: '#{ filename }' does not exist"
 end
