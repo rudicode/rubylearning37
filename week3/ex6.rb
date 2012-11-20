2c4e8af2145c6226eda303dd0de23eccc88afa51
Rudi Luzar <rudi@rudicode.com>
2012-11-20 17:39:42 -0500

    week3 ex6 -- added new method talking_money? to see if granny heard anything about the cash

--- week3/ex6.rb
+++ week3/ex6.rb
@@ -5,17 +5,22 @@ def yelling? string
     string == string.upcase if string.index(%r/[a-z]|[A-Z]/)
 end
 
-def grandma_remembers
-   years = (1930..1950).to_a
-   years[rand(years.size)]
+def talking_money? string
+  string.index(%r/\$\d+/)
+end
+
+def grandma_says
+   (1930..1950).to_a.sample
 end
 
 until "BYE" == (sonny_says = prompt('You say to Grandma').strip)
   print "Grandma responds: "
-  if yelling? sonny_says
-
-    puts "NO, NOT SINCE #{ grandma_remembers }!"
+  if talking_money? sonny_says
+    puts "STASH THE CASH IN THE COOKIE JAR!"
+  elsif yelling? sonny_says
+    puts "NO, NOT SINCE #{ grandma_says }!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
+  puts
 end
