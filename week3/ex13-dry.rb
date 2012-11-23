7dbd2a3448ee104355c49ebd7f5b3ed2b327a989
Rudi Luzar <rudi@rudicode.com>
2012-11-23 08:44:41 -0500

    week3 ex13 -- One line Hash

---
+++ week3/ex13-dry.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+#
+record = { :name => "Satish", :email => "mail@satishtalim.com", :phone => "919371006659" }
+another_record = { name: "Satish", email: "mail@satishtalim.com", phone: "919371006659" }
+
+puts record
+puts another_record
