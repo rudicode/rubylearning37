c046561de595346b7fde25802e25adb23ac4573b
Victor Goff <keeperotphones@gmail.com>
2012-11-24 18:11:03 -0600

    fixed variable names, and added heredoc for output

--- 4week/dog.rb
+++ 4week/dog.rb
@@ -1,18 +1,18 @@
 class Dog
-  
+
   attr_reader :name
-  
+
   def initialize name
     @name = name
   end
   def bark
     "Bark!"
   end
-  
+
   def eat
     "Mmm, eat Scooby snacks!"
   end
-  
+
   def chase_cat
     "Get that cat!"
   end
