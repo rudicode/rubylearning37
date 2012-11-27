a84310db6fcb8855a6c4c0d3e992075c569e7f0b
Rudi Luzar <rudi@rudicode.com>
2012-11-26 22:45:29 -0500

    week3 ex2 -- Updated usage into it's own method

--- week3/ex2.rb
+++ week3/ex2.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 #
 require 'tempfile'
-
+#
 def insert_word str, find, insert
   output = "#{insert} #{find}"
   str.gsub(find, output)
 end
-
+#
 def update_file filename, find, insert
   puts "Updating file: #{ filename } "
 
@@ -24,24 +24,26 @@ def update_file filename, find, insert
   File.rename(file_out.path, filename)
 end
 #
-unless ARGV.length == 3 && File.exist?(ARGV[0])
-  puts <<LIST_INSTRUCTIONS
-
-Inserts given text before each occurance of found text.
-
-Usage: ex2.rb FILE find insert
-
-       FILE       the file to update
-       find       the word to find in file
-       insert     the word that replaces each occurance of find
-
-example:
-         ex2.rb ex2-words.txt word inserted
-
+def usage
+  unless ARGV.length == 3 && File.exist?(ARGV[0])
+    puts <<LIST_INSTRUCTIONS
+  
+  Inserts given text before each occurance of found text.
+  
+  Usage: ex2.rb FILE find insert
+  
+         FILE       the file to update
+         find       the word to find in file
+         insert     the word that replaces each occurance of find
+  
+  example:
+           ex2.rb ex2-words.txt word inserted
+  
 LIST_INSTRUCTIONS
-  exit 1
+    exit 1
+  end
 end
-
-filename, find, insert = ARGV
 #
+usage
+filename, find, insert = ARGV
 update_file(filename, find, insert)
