f18720c9d7c3860d0a01ad5798cfb9a9d9edeef9
Victor Goff <keeperotphones@gmail.com>
2012-11-29 00:44:24 -0600

    small proposed change for "end of data" idea

--- 4week/playfair/roller.rb
+++ 4week/playfair/roller.rb
@@ -1,22 +1,26 @@
 class Roller
-  
+
   def initialize values
     @data = values.to_a
     @position = -1 # first call to next will return first elemet
   end
-  
+
   def all
     @data
   end
-  
+
   def current
     @data[@position]
   end
-  
+
   def next
-    @position += 1
-    @position = 0 if @position == @data.length
+    @position += 1 and eod and @position = 0
     @data[@position]
   end
-  
+
+  private
+    def eod
+      @position == @data.length
+    end
+
 end
