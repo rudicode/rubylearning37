80806148c483399560f65c7eb0132f141991b474
Victor Goff <keeperotphones@gmail.com>
2012-12-04 05:39:21 -0600

    See long message - Custom Reporters, started tuning tests In this change, I am using the custom reporters, holding the options in an array and using them randomly.
    Also, I have started fine tuning the tests to conform with original specs.

--- 5week/gameboard_spec.rb
+++ 5week/gameboard_spec.rb
@@ -1,4 +1,15 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+reporters = [ MiniTest::Reporters::DefaultReporter.new,
+              MiniTest::Reporters::SpecReporter.new, 
+              MiniTest::Reporters::ProgressReporter.new,
+              # MiniTest::Reporters::RubyMineReporter.new,
+              # MiniTest::Reporters::RubyMateReporter.new,
+              # MiniTest::Reporters::GuardReporter.new,
+              ]
+MiniTest::Reporters.use! [reporters.sample]
+
 require_relative('gameboard')
 
 describe "GameBoard: " do
@@ -8,17 +19,17 @@ describe "GameBoard: " do
     gb.set_locations_cells(locations).must_equal [2,3,4]
   end
 
-  it "should test answer" do
+  it "should report hits" do
     gb = GameBoard.new
     locations = [2,3,4]
     gb.set_locations_cells(locations)
-    gb.check_yourself(0).must_equal "MISS"
-    gb.check_yourself(1).must_equal "MISS"
-    gb.check_yourself(5).must_equal "MISS"
-    gb.check_yourself(6).must_equal "MISS"
-    gb.check_yourself(2).must_equal "HIT"
-    gb.check_yourself(3).must_equal "HIT"
-    gb.check_yourself(4).must_equal "KILL"
+    gb.check_yourself(0).must_equal "Miss"
+    gb.check_yourself(1).must_equal "Miss"
+    gb.check_yourself(5).must_equal "Miss"
+    gb.check_yourself(6).must_equal "Miss"
+    gb.check_yourself(2).must_equal "Hit"
+    gb.check_yourself(3).must_equal "Hit"
+    gb.check_yourself(4).must_equal "END"
   end
 
 end
