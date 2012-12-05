4f924ce8411529e9fc7d35dbe56c211f52e411fa
Victor Goff <keeperotphones@gmail.com>
2012-12-05 01:22:58 -0600

    organized tests for gameboard and added client upgrade tests

--- 5week/test/gameboard_spec.rb
+++ 5week/test/gameboard_spec.rb
@@ -8,28 +8,49 @@ reporters = [ MiniTest::Reporters::DefaultReporter.new,
               # MiniTest::Reporters::RubyMateReporter.new,
               # MiniTest::Reporters::GuardReporter.new,
               ]
-MiniTest::Reporters.use! [reporters[2]]
+MiniTest::Reporters.use! [reporters[1]]
 
 require './lib/gameboard'
 
 describe "GameBoard: " do
-  it "should contain" do
-    gb = GameBoard.new
-    locations = [2,3,4] 
-    gb.set_locations_cells(locations).must_equal [2,3,4]
-  end
-
-  it "should report hits" do
-    gb = GameBoard.new
+  before :each do
+    @gb = GameBoard.new
     locations = [2,3,4]
-    gb.set_locations_cells(locations)
-    gb.check_yourself(0).must_equal "Miss"
-    gb.check_yourself(1).must_equal "Miss"
-    gb.check_yourself(5).must_equal "Miss"
-    gb.check_yourself(6).must_equal "Miss"
-    gb.check_yourself(2).must_equal "Hit"
-    gb.check_yourself(3).must_equal "Hit"
-    gb.check_yourself(4).must_equal "kill"
+    @gb.set_locations_cells(locations)
+  end
+  it "should contain" do
+    @gb = GameBoard.new
   end
 
+  it "should report misses" do
+    @gb.check_yourself(0).must_equal "Miss"
+    @gb.check_yourself(1).must_equal "Miss"
+    @gb.check_yourself(5).must_equal "Miss"
+    @gb.check_yourself(6).must_equal "Miss"
+  end
+  it "should report hits" do
+    @gb.check_yourself(2).must_equal "Hit"
+    @gb.check_yourself(3).must_equal "Hit"
+  end
+  it "should miss if same target is called" do
+    @gb.check_yourself(2).must_equal "Hit"
+    @gb.check_yourself(2).must_equal "Miss"
+  end
+  it "should end the game when locations are empty" do
+    @gb.check_yourself(2).must_equal "Hit"
+    @gb.check_yourself(3).must_equal "Hit"
+    @gb.check_yourself(4).must_equal "kill"
+  end
 end
+
+describe "Gameboard ready for client upgrades" do
+  before :each do
+    @gb = GameBoard.new
+    locations = [2,3,4]
+    @gb.set_locations_cells(locations)
+  end
+  it "should work with larger playing field"
+  it "should work with non-sequntial targets"
+  it "should work with string targets"
+end
+
