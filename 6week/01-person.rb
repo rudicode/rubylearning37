71dde0606ac6182ce1c4d957efd28ba1b93ef2fe
Rudi Luzar <rudi@rudicode.com>
2012-12-14 07:56:00 -0500

    week 6 ex01 a different way to create a class

--- 6week/01-person.rb
+++ 6week/01-person.rb
@@ -8,5 +8,30 @@ class Person
   end
 end
 
-mike = Person.new()
-puts mike.balance
+begin
+  mike = Person.new()
+  puts mike.balance
+  mike.balance = "$10.00"
+  puts mike.balance
+rescue Exception => e
+  puts e
+end
+#
+# A different way
+#
+puts "A different way to define a class"
+person = Class.new do
+  attr_reader :balance
+  def initialize balance = "$65.23"
+    @balance = balance
+  end
+end
+#
+begin
+  tom = person.new()
+  puts tom.balance
+  tom.balance = "$15.00"
+  puts tom.balance
+rescue Exception => e
+  puts e
+end
