1c3c69305cd3bfdd364e5a1fe48fd24dadf8e3f3
Rudi <rudi@rudicode.com>
2012-12-22 14:26:27 -0500

    week7 exercises starting

---
+++ 7week/bin/month_days.rb
@@ -0,0 +1,10 @@
+require 'date'
+
+def month_days month, year = nil
+  # use Date 
+  
+end
+
+days = month_days(5) # 31 (May)
+
+days = month_days(2, 2000) # 29 (February 2000)
---
+++ 7week/notes/p052dumpgc.rb
@@ -0,0 +1,11 @@
+# p052dumpgc.rb  
+require_relative 'p051gamecharacters'  
+gc = GameCharacter.new(120, 'Magician', ['spells', 'invisibility'])  
+puts "#{gc.power} #{gc.type}"  
+gc.weapons.each do |w|  
+  puts w  
+end  
+  
+File.open('game', 'w+') do |f|  
+  Marshal.dump(gc, f)  
+end
